5b037989a244f34d2e979879c409f2e12aea9e51
Suguru Odai <ourqwers@gmail.com>
2015-02-16 21:22:01 +0900

    finish

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,20 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def calculate
+    @input = params[:expression] 
+    
+    if @input =~ /^[0-9-+\/\*()\.]+$/
+      begin
+        @result = eval(@input)
+      rescue SyntaxError
+        @result = "invalid expression!"
+      end
+    else
+      @result = "invalid expression!"
+    end
+
+    render :home
+  end
 end
