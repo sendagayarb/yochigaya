34cbc4959a7f5cb2c262e153dd1ffb25fac08969
chck <iwazaki_yuki@cyberagent.co.jp>
2015-02-17 00:16:20 +0900

    add dream

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
+require_relative "../services/analyze-follower"
+
 class StaticPagesController < ApplicationController
   def home
   end
 
   def help
+  end
+
+  def analyze
+    @id = params[:twitter_id]
+    af = AnalyzeFollower.new
+    @texts = af.get_fw_kw(@id)
   end
 end
